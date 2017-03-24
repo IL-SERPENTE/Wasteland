6e0af9bf088364622e0c273bee7267015e7d36aa
Werter <shai.halimi@gmail.com>
2017-03-24 19:41:38 +0100

    Fix turret maybe will change

--- src/main/java/net/samagames/entity/Turret.java
+++ src/main/java/net/samagames/entity/Turret.java
@@ -1,29 +1,58 @@
 package net.samagames.entity;
 
+import net.samagames.Wasteland;
 import net.samagames.player.Team;
+import net.samagames.player.WastelandPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.*;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * Created by werter on 21.03.2017.
  */
 public class Turret {
+
+    private Wasteland wasteland;
     private Team team;
     private Location location;
     private int range;
 
-    public Turret (Team team, Location location, int range){
+    public Turret(Wasteland wasteland, Team team, Location location, int range) {
+        this.wasteland = wasteland;
         this.team = team;
         this.location = location;
         this.range = range;
     }
 
 
+    private BukkitRunnable bukkitRunnable;
+
 
     public void enable(){
-        //TODO
+        bukkitRunnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
+                    if (wastelandPlayer.hasTeam())
+                        if (!wastelandPlayer.getTeam().equals(team) && location.distance(player.getLocation()) <= range) {
+                            ShulkerBullet shulkerBullet = player.getWorld().spawn(location,ShulkerBullet.class);
+                            shulkerBullet.setTarget(player);
+                        }
+                }
+            }
+        };        bukkitRunnable.runTaskTimer(wasteland.getMain(),20,100);
+
+    }
+
+    public void disable(){
+        bukkitRunnable.cancel();
     }
 
     public void init(){
-        //TODO
+        location.getBlock().setType(Material.AIR);
+
     }
 }
