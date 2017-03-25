b8d0d99263c107a076683d002782ca47e3042f7d
Werter <shai.halimi@gmail.com>
2017-03-25 17:15:15 +0100

    Add harvest by player management

--- src/main/java/net/samagames/Listerner/PlayerEvent.java
+++ src/main/java/net/samagames/Listerner/PlayerEvent.java
@@ -2,10 +2,14 @@ package net.samagames.Listerner;
 
 import net.samagames.Wasteland;
 import net.samagames.WastelandItem;
+import net.samagames.player.WastelandPlayer;
+import net.samagames.tools.chat.ActionBarAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -16,6 +20,25 @@ public class PlayerEvent implements Listener {
 
     public PlayerEvent (Wasteland wasteland){
         this.wasteland = wasteland;
+    }
+
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent event){
+        event.setCancelled(true);
+        Player player = event.getPlayer();
+        WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
+        int wheat = wastelandPlayer.getWheat();
+        int amount = event.getItem().getItemStack().getAmount();
+        if(wheat < 50) {
+            event.getItem().remove();
+            if (wheat + amount > 50) {
+                wastelandPlayer.setWheat(50);
+                ItemStack itemStack = event.getItem().getItemStack();
+                itemStack.setAmount(wheat + amount - 50);
+                player.getWorld().dropItem(event.getItem().getLocation(), itemStack);
+            } else
+                wastelandPlayer.addWheat(amount);
+        }
     }
 
     @EventHandler
