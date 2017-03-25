7ac855078f1ccb9c2ac3af3f01cd82242d6d96f9
Werter <shai.halimi@gmail.com>
2017-03-25 22:38:03 +0100

    Add name to team color

--- src/main/java/net/samagames/player/TeamColor.java
+++ src/main/java/net/samagames/player/TeamColor.java
@@ -7,13 +7,17 @@ import org.bukkit.ChatColor;
  */
 public enum TeamColor{
 
-    RED(ChatColor.RED),BLUE(ChatColor.BLUE);
+    RED(ChatColor.RED,"rouge"),BLUE(ChatColor.BLUE,"bleu");
 
     private ChatColor chatColor;
+    private String name;
 
-    TeamColor(ChatColor color){
+    TeamColor(ChatColor color,String name){
         this.chatColor = color;
+        this.name = name;
     }
+
+    public String getName() { return  this.name;}
 
     public ChatColor getChatColor() {
         return chatColor;
