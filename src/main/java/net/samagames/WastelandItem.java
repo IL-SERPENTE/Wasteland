a40f11970febf1eacd9c11a48e70b86b1ba01ec2
Werter <shai.halimi@gmail.com>
2017-03-29 13:08:13 +0200

    Change item Name and lore systeme

--- src/main/java/net/samagames/WastelandItem.java
+++ src/main/java/net/samagames/WastelandItem.java
@@ -1,6 +1,7 @@
 package net.samagames;
 
 import net.samagames.api.SamaGamesAPI;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -14,22 +15,20 @@ import java.util.List;
 public enum WastelandItem {
     LEAVE(SamaGamesAPI.get().getGameManager().getCoherenceMachine().getLeaveItem(),"Leave",8 ,true,"leave"),
     KIT_SELECTOR(new ItemStack(Material.ENDER_CHEST), "kit",4 ,true ,"kit"),
-    JOIN_TEAM_BLUE(new ItemStack(Material.BANNER,1, (byte) 4),"Bleu",0 ,true,"Bleu"),
-    JOIN_TEAM_RED(new ItemStack(Material.BANNER,1,(byte)1),"Rouge",1,true,"Rouge");
-
+    JOIN_TEAM_BLUE(new ItemStack(Material.BANNER,1, (byte) 4),ChatColor.BLUE + "Bleu",0 ,true,ChatColor.BLUE + "rejoindre léquipe bleu"),
+    JOIN_TEAM_RED(new ItemStack(Material.BANNER,1,(byte)1),ChatColor.RED + "Rouge",1,true, ChatColor.RED + "rejoinde l'équipe rouge");
 
     private ItemStack itemStack;
     private String name;
     private int slot;
     private boolean starterItem;
-    private String[] lore;
+    private String lore;
 
 
-    WastelandItem(ItemStack itemStack ,String name,Integer slot ,boolean starterItem ,String... lore){
+    WastelandItem(ItemStack itemStack ,String name,Integer slot ,boolean starterItem ,String lore){
         ItemMeta itemMeta = itemStack.getItemMeta();
         List<String> listLore = new ArrayList<>();
-        for(String stringLore : lore)
-            listLore.add(stringLore);
+        listLore.add(lore);
         itemMeta.setLore(listLore);
         itemMeta.setDisplayName(name);
         itemStack.setItemMeta(itemMeta);
@@ -49,7 +48,7 @@ public enum WastelandItem {
         return slot;
     }
 
-    public String[] getLore() {
+    public String getLore() {
         return lore;
     }
 
