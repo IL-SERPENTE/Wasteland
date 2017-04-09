0b7823494129845dc9c2235b0977c61e3e0c4408
Werter <shai.halimi@gmail.com>
2017-04-09 12:37:53 +0200

    Add color to TeamColor

--- src/main/java/net/samagames/player/TeamColor.java
+++ src/main/java/net/samagames/player/TeamColor.java
@@ -1,25 +1,33 @@
 package net.samagames.player;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 
 /**
  * Created by werter on 21.03.2017.
  */
 public enum TeamColor{
 
-    RED(ChatColor.RED,"rouge"),BLUE(ChatColor.BLUE,"bleu");
+    RED(ChatColor.RED,Color.RED,"rouge"),BLUE(ChatColor.BLUE,Color.BLUE,"bleu");
 
     private ChatColor chatColor;
+    private Color color;
     private String name;
 
-    TeamColor(ChatColor color,String name){
-        this.chatColor = color;
+    TeamColor(ChatColor chatColor, Color color, String name){
+        this.chatColor = chatColor;
+        this.color = color;
         this.name = name;
     }
+
 
     public String getName() { return  this.name;}
 
     public ChatColor getChatColor() {
         return chatColor;
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
