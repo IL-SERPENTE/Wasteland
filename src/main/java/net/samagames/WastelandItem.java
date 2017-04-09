424a49ce35d9d3f8e1bd82ff0742a996b117b105
Werter <shai.halimi@gmail.com>
2017-04-09 02:52:43 +0200

    remove laste commit

--- src/main/java/net/samagames/WastelandItem.java
+++ src/main/java/net/samagames/WastelandItem.java
@@ -18,13 +18,11 @@ public enum WastelandItem {
     KIT_SELECTOR(new ItemStack(Material.ENDER_CHEST), "kit",4 ,true ,"kit"),
     JOIN_TEAM_BLUE(new ItemStack(Material.BANNER,1, (byte) 4),ChatColor.BLUE + "Bleu",0 ,true,ChatColor.BLUE + "rejoindre léquipe bleu"),
     JOIN_TEAM_RED(new ItemStack(Material.BANNER,1,(byte)1),ChatColor.RED + "Rouge",1,true, ChatColor.RED + "rejoinde l'équipe rouge"),
-
-
-    CHOOSE_KIT_DEFENDER(new ItemStack(Material.BOW), "Choisir le kit defenseur",9, false, "Ce kit vous permet d'avoir plus de fleche"),
-    CHOOSE_KIT_DEMOLISHER(new ItemStack(Material.IRON_AXE), "Choisir le kit demolisseur",11 ,false , "Lorsque que le joueur ressuscite sa résistance est augmentée, il reçoit donc des coeurs d'absorptions pendant 30 secondes."),
-    CHOOSE_KIT_HERBALIST(new ItemStack(Material.LONG_GRASS),"Choisir le kit Herboriste", 13 , false , "Lorsqu’un joueur tue un ennemi, la chance qu’il a de recevoir des plantes dans son inventaire est augmenté."),
-    CHOOSE_KIT_ROBBER(new ItemStack(Material.YELLOW_FLOWER), "Choisir le kit Voleur", 15, false ,"Lorsque le joueur pille le camp adverse, la chance qu’il a de voler un plus grand nombre de ressource est augmenté."),
-    CHOOSE_KIT_TRAPPER(new ItemStack(Material.WEB), "Choisir le kit trappeur", 17, false , "Vous avez plus de toiles d'araignées.");
+    CHOOSE_KIT_DEFENDER(new ItemStack(Material.BOW), "Defenseur",9, false, "Ce kit vous permet d'avoir plus de fleche"),
+    CHOOSE_KIT_DEMOLISHER(new ItemStack(Material.IRON_AXE), "Demolisseur",11 ,false , "Lorsque que le joueur ressuscite sa résistance est augmentée, il reçoit donc des coeurs d'absorptions pendant 30 secondes."),
+    CHOOSE_KIT_HERBALIST(new ItemStack(Material.LONG_GRASS),"Herboriste", 13 , false , "Lorsqu’un joueur tue un ennemi, la chance qu’il a de recevoir des plantes dans son inventaire est augmenté."),
+    CHOOSE_KIT_ROBBER(new ItemStack(Material.YELLOW_FLOWER), "Voleur", 15, false ,"Lorsque le joueur pille le camp adverse, la chance qu’il a de voler un plus grand nombre de ressource est augmenté."),
+    CHOOSE_KIT_TRAPPER(new ItemStack(Material.WEB), "Trappeur", 17, false , "Vous avez plus de toiles d'araignées.");
 
     private ItemStack itemStack;
     private String name;
@@ -45,26 +43,6 @@ public enum WastelandItem {
         this.slot = slot;
         this.starterItem = starterItem;
         this.itemStack = itemStack;
-    }
-
-    public static boolean isWastelandItem(ItemStack itemStack){
-        boolean isWastelandItem = false;
-        for(WastelandItem wastelandItem : WastelandItem.values())
-            if(wastelandItem.getItemStack().equals(itemStack)){
-                isWastelandItem = true;
-                break;
-            }
-        return isWastelandItem;
-    }
-
-    public static WastelandItem getByItemStack(ItemStack itemStack){
-        WastelandItem wastelandItem = null;
-        for(WastelandItem wastelandItems : WastelandItem.values())
-            if(wastelandItem.getItemStack().equals(itemStack)){
-                wastelandItem = wastelandItems;
-                break;
-            }
-        return  wastelandItem;
     }
 
     public boolean isStarterItem() {
