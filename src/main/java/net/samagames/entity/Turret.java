0ccc672c9797a7df587462c31806a27a83d9038f
Werter <shai.halimi@gmail.com>
2017-04-17 13:34:02 +0200

    Remove a bukkitRunnable and add turret capture

--- src/main/java/net/samagames/entity/Turret.java
+++ src/main/java/net/samagames/entity/Turret.java
@@ -4,19 +4,13 @@ import net.samagames.Wasteland;
 import net.samagames.player.Team;
 import net.samagames.player.WastelandPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.EntityEffect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.*;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
 
 /**
  * Created by werter on 21.03.2017.
@@ -28,6 +22,7 @@ public class Turret implements Listener{
     private Team team;
     private Location location;
     private int range;
+    private HashMap<Player,Integer> playerInTurret = new HashMap<>();
 
     public Turret(){}
 
@@ -44,15 +39,15 @@ public class Turret implements Listener{
 
 
     public void enable(){
-        new BukkitRunnable(){
-            @Override
-            public void run() {
-                for(Location location : locations) new Plant(location);
-            }
-        }.runTaskTimer(wasteland.getMain(),20,20*120);
         bukkitRunnable = new BukkitRunnable() {
+            int seconds = 0;
             @Override
             public void run() {
+                seconds = seconds + 5;
+                if(seconds == 120) {
+                    for (Location location : locations) new Plant(location);
+                    seconds = 0;
+                }
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
                     if (wastelandPlayer.hasTeam())
@@ -62,7 +57,27 @@ public class Turret implements Listener{
                 }
             }
         };        bukkitRunnable.runTaskTimer(wasteland.getMain(),20,100);
-
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for(Player player : Bukkit.getOnlinePlayers()) {
+                    WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
+                    if (!wastelandPlayer.getTeam().equals(team) && location.distance(player.getLocation()) <= range) {
+                        if(playerInTurret.get(player) != null && playerInTurret.get(player) == 10) {
+                            team = wastelandPlayer.getTeam();
+                            player.sendMessage("Vous avez capturé l'avant garde");
+                            return;
+                        }
+                        if (playerInTurret.containsKey(player))
+                            playerInTurret.put(player, playerInTurret.get(player) + 1);
+                        else
+                            playerInTurret.put(player, 1);
+                        if(playerInTurret.get(player) == 10 || playerInTurret.get(player) == 5 || playerInTurret.get(player) < 4)
+                            player.sendMessage("Restez encore " + (10 - playerInTurret.get(player)) + " dans l'avant garde pour le capturer.");
+                    }
+                }
+            }
+        }.runTaskTimer(wasteland.getMain(),20,20);
     }
 
     public void disable(){
