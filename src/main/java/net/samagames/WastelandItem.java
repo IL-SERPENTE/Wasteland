dc85f6b4944e4aa0fb5f32b0a811cbf758fd60c8
Werter <shai.halimi@gmail.com>
2017-05-06 23:01:37 +0200

    Add team selector item

--- src/main/java/net/samagames/WastelandItem.java
+++ src/main/java/net/samagames/WastelandItem.java
@@ -16,8 +16,9 @@ public enum WastelandItem {
 
     LEAVE(SamaGamesAPI.get().getGameManager().getCoherenceMachine().getLeaveItem(),"Leave",8 ,true,"leave"),
     KIT_SELECTOR(new ItemStack(Material.ENDER_CHEST), "kit",4 ,true ,"kit"),
-    JOIN_TEAM_BLUE(new ItemStack(Material.BANNER,1, (byte) 4),ChatColor.BLUE + "Bleu",0 ,true,ChatColor.BLUE + "rejoindre léquipe bleu"),
-    JOIN_TEAM_RED(new ItemStack(Material.BANNER,1,(byte)1),ChatColor.RED + "Rouge",1,true, ChatColor.RED + "rejoinde l'équipe rouge"),
+    TEAM_SELECTOR(new ItemStack(Material.BANNER,1,(byte)15),"Choisir une équipe",0,true,"Cliquez pour rejoindre une équipe."),
+    JOIN_TEAM_BLUE(new ItemStack(Material.BANNER,1, (byte) 4),ChatColor.BLUE + "Bleu",0 ,false,ChatColor.BLUE + "rejoindre léquipe bleu"),
+    JOIN_TEAM_RED(new ItemStack(Material.BANNER,1,(byte)1),ChatColor.RED + "Rouge",4,false, ChatColor.RED + "rejoinde l'équipe rouge"),
     CHOOSE_KIT_DEFENDER(new ItemStack(Material.BOW), "Defenseur",9, false, "Ce kit vous permet d'avoir plus de fleche"),
     CHOOSE_KIT_DEMOLISHER(new ItemStack(Material.IRON_AXE), "Demolisseur",11 ,false , "Lorsque que le joueur ressuscite","sa résistance est augmentée, il","reçoit donc des coeurs d'absorptions","pendant 30 secondes."),
     CHOOSE_KIT_HERBALIST(new ItemStack(Material.LONG_GRASS),"Herboriste", 13 , false , "Lorsqu’un joueur tue un ennemi, la","chance qu’il a de recevoir","des plantes dans son inventaire","est augmenté."),
