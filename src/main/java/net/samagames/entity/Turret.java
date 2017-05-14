75debc546a01c73a6bed03dda6196cb880e2b533
werter <shai.halimi@gmail.com>
2017-05-14 16:23:18 +0200

    Add particles when turret shoot

--- src/main/java/net/samagames/entity/Turret.java
+++ src/main/java/net/samagames/entity/Turret.java
@@ -3,15 +3,14 @@ package net.samagames.entity;
 import net.samagames.Wasteland;
 import net.samagames.player.Team;
 import net.samagames.player.WastelandPlayer;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by werter on 21.03.2017.
@@ -55,6 +54,7 @@ public class Turret implements Listener{
                     if (wastelandPlayer.hasTeam())
                         if (!wastelandPlayer.getTeam().equals(team) && location.distance(player.getLocation()) <= range) {
                             player.damage(4);
+                            shoot(player.getLocation());
                         }
                 }
             }
@@ -63,10 +63,15 @@ public class Turret implements Listener{
             @Override
             public void run() {
                 for(Player player : Bukkit.getOnlinePlayers()) {
+                    if(getEnnemiesInTurret() < getTeamInTurret()){
+                        playerInTurret.forEach((k,v)->playerInTurret.remove(k));
+                        break;
+                    }
                     WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
                     if (!wastelandPlayer.getTeam().equals(team) && location.distance(player.getLocation()) <= range) {
                         if(playerInTurret.get(player) != null && playerInTurret.get(player) == 10) {
                             team = wastelandPlayer.getTeam();
+                            player.playSound(player.getLocation(),Sound.ENTITY_PLAYER_LEVELUP,1,1);
                             player.sendMessage("Vous avez capturé l'avant garde");
                             playerInTurret.remove(player);
                             return;
@@ -82,6 +87,34 @@ public class Turret implements Listener{
                 }
             }
         }.runTaskTimer(wasteland.getMain(),20,20);
+    }
+
+    public void shoot (Location location){
+        Vector vector = location.clone().subtract(this.location).toVector().normalize().multiply(0.3);
+        for (int n = (int) location.distance(this.location) * 10/3; n >= 0; n--) {
+            this.location.add(vector);
+            location.getWorld().spawnParticle(Particle.FLAME , this.location.getX(),this.location.getY(),this.location.getZ(),1,0,0,0,0);
+        }
+    }
+
+    public int getTeamInTurret(){
+        int playerIntTurret = 0;
+        for(Player player : Bukkit.getOnlinePlayers()){
+            WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
+            if(wastelandPlayer.getTeam() == this.team && player.getLocation().distance(this.location) <= range)
+                playerIntTurret++;
+        }
+        return playerIntTurret;
+    }
+
+    public int getEnnemiesInTurret(){
+        int playerIntTurret = 0;
+        for(Player player : Bukkit.getOnlinePlayers()){
+            WastelandPlayer wastelandPlayer = wasteland.getWastelandPlayer(player);
+            if(wastelandPlayer.getTeam() != this.team && player.getLocation().distance(this.location) <= range)
+                playerIntTurret++;
+        }
+        return playerIntTurret;
     }
 
     public void disable(){
